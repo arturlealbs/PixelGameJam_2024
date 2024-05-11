1ff7504cacb3f5b56b91921b382bf681ad3f01ec
Keu <keuxd2003@gmail.com>
2024-05-11 20:09:52 -0300

    Player is now using rigidbody physics

--- Assets/Code/Scripts/CSharp/PlayerMovement.cs
+++ Assets/Code/Scripts/CSharp/PlayerMovement.cs
@@ -1,15 +1,16 @@
 using Godot;
 using System;
 
-public partial class PlayerMovement : Node2D
+public partial class PlayerMovement : CharacterBody2D
 {
 
-	public int speed = 2;
+	public int speed = 100;
 
     public override void _PhysicsProcess(double delta)
     {
         Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
-		Position += inputDirection * speed;
+		Velocity = inputDirection * speed;
+		MoveAndSlide();
     }
 
 }
