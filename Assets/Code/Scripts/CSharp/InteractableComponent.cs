4662c8d970a6c230ffafe63e7ff6c849505474ee
Keu <keuxd2003@gmail.com>
2024-05-13 18:06:40 -0300

    Create InteractableComponent/OnInteraction signal

---
+++ Assets/Code/Scripts/CSharp/InteractableComponent.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class InteractableComponent : Node
+{
+	
+	[Signal]
+	public delegate void OnInteractionEventHandler();
+
+	private Area2D hitbox;
+	private Label label;
+
+	public override void _Ready()
+	{
+		hitbox = GetChild<Area2D>(0);
+		hitbox.BodyEntered += PlayerEntered;
+		hitbox.BodyExited += PlayerExited;
+		label = GetChild<Label>(1);
+		label.Text = InputMap.ActionGetEvents("interact")[0].AsText().Split(" ")[0];
+	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if(Input.IsActionJustPressed("interact") && label.Visible)
+        {
+			EmitSignal(SignalName.OnInteraction);
+		}
+    }
+
+    private void PlayerEntered(Node2D player)
+	{
+		label.Visible = true;
+	}
+
+	private void PlayerExited(Node2D player)
+	{
+		label.Visible = false;
+	}
+
+}
