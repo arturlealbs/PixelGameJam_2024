41cc036c5f79ac5de3aa75bf167c288e96943038
Keu <keuxd2003@gmail.com>
2024-05-13 18:07:58 -0300

    Speed is now exported, Move is a now in a method

--- Assets/Code/Scripts/CSharp/PlayerMovement.cs
+++ Assets/Code/Scripts/CSharp/PlayerMovement.cs
@@ -3,10 +3,15 @@ using System;
 
 public partial class PlayerMovement : CharacterBody2D
 {
-
+	[Export]
 	public int speed = 100;
 
 	public override void _PhysicsProcess(double delta)
+	{
+		MoveTick();
+	}
+
+	private void MoveTick()
 	{
 		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
 		Velocity = inputDirection * speed;
